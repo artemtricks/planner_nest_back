485e1caebd114bf7bca547ca95bbf8cf8aa57cbb
Artem <artemtricks@yandex.ru>
2024-06-13 19:03:56 +0700

    wip auth and register

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma.service';
+import { AuthDto } from 'src/auth/dto/auth.dto';
+import { hash } from 'argon2';
 
 @Injectable()
 export class UserService {
@@ -15,6 +17,26 @@ export class UserService {
       include: {
         tasks: true,
       },
+    });
+  }
+
+  getByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async create(dto: AuthDto) {
+    const user = {
+      email: dto.email,
+      name: '',
+      password: await hash(dto.password),
+    };
+
+    return this.prisma.user.create({
+      data: user,
     });
   }
 }
