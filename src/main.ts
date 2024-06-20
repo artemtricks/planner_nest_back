3c37289b569815d9eb36303710c969bc991f2e8d
Artem <artemtricks@yandex.ru>
2024-06-20 19:51:08 +0700

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cors = require('cors');
   app.setGlobalPrefix('api');
-  app.use(cookieParser());
+  const corsOptions = {
+    origin: 'http://localhost:3001',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+  app.use([cookieParser(), cors(corsOptions)]);
   app.enableCors({
     origin: ['http://localhost:3001/'],
     credentials: true,
